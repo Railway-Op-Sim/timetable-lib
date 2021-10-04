4e730efc3b122d98e75de06975dc037f151a2829
Daniel-Gill <56364196+Daniel-Gill@users.noreply.github.com>
2021-10-04 18:39:43 +0100

    Location now stores as strings
    Should fix the Nxx issues

--- src/main/java/net/danielgill/ros/service/location/Location.java
+++ src/main/java/net/danielgill/ros/service/location/Location.java
@@ -1,13 +1,13 @@
 package net.danielgill.ros.service.location;
 
 public class Location {
-    private final int xID;
-    private final int yID;
+    private final String xID;
+    private final String yID;
     
     public Location(String location) {
         String[] split = location.split("-");
-        xID = Integer.parseInt(split[0]);
-        yID = Integer.parseInt(split[1]);
+        xID = split[0];
+        yID = split[1];
     }
     
     @Override
