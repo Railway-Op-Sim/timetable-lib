85e448d5a3d1eb0efd0d9bd0b199f18ad5aee72f
Daniel-Gill <d.gill@se20.qmul.ac.uk>
2021-10-25 10:28:12 +0100

    Added comments to more classes

--- src/main/java/net/danielgill/ros/service/event/TimedEvent.java
+++ src/main/java/net/danielgill/ros/service/event/TimedEvent.java
@@ -1,9 +1,17 @@
 package net.danielgill.ros.service.event;
 
+/**
+ * An event class that represents any event which has a time value.
+ * @author Daniel Gill
+ */
 public abstract class TimedEvent extends Event {
     public TimedEvent(String type) {
         super(type);
     }
-    
+
+    /**
+     * Increments the time in the event by a given number of minutes.
+     * @param minutes The minutes to be added to the time on the event.
+     */
     public abstract void incrementTime(int minutes);
 }
