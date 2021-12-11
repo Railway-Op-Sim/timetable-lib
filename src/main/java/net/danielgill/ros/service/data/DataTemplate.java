bdad847270ff471d376b14a0a9e1472be5c3e6da
Daniel-Gill <d.gill@se20.qmul.ac.uk>
2021-12-11 23:57:26 +0000

    Updated and added javadoc

--- src/main/java/net/danielgill/ros/service/data/DataTemplate.java
+++ src/main/java/net/danielgill/ros/service/data/DataTemplate.java
@@ -1,18 +1,39 @@
 package net.danielgill.ros.service.data;
 
+/**
+ * A template for containing service data, identified with a keyword.
+ * @author Daniel Gill
+ */
 public class DataTemplate {
     private Data data;
     private String keyword;
     
+    /**
+     * Create a new data template from individual service data and a keyword.
+     * @param keyword The keyword for the data template
+     * @param maxSpeed
+     * @param mass
+     * @param maxBrake
+     * @param power 
+     */
     public DataTemplate(String keyword, int maxSpeed, int mass, int maxBrake, int power) {
         this.data = new Data(maxSpeed, mass, maxBrake, power);
         this.keyword = keyword;
     }
     
+    /**
+     * Gets the data included in a single data template.
+     * @return The service data
+     */
     public Data getData(){
         return data;
     }
     
+    /**
+     * Checks if the keyword is equal to a given string.
+     * @param keywordTest The string to be checked against the keyword.
+     * @return A boolean for whether they are equal.
+     */
     public boolean keywordEqual(String keywordTest) {
         return this.keyword.equalsIgnoreCase(keywordTest);
     }
--- src/main/java/net/danielgill/ros/service/data/DataTemplates.java
+++ src/main/java/net/danielgill/ros/service/data/DataTemplates.java
@@ -2,23 +2,53 @@ package net.danielgill.ros.service.data;
 
 import java.util.ArrayList;
 
+/**
+ * Contains a list of data templates, and also allows the user to add their own data templates.
+ * @author Daniel Gill
+ */
 public class DataTemplates {
     public ArrayList<DataTemplate> templates;
     
+    /**
+     * Adds a data template created from the keyword and data points.
+     * @param keyword
+     * @param maxSpeed
+     * @param mass
+     * @param maxBrake
+     * @param power 
+     */
     public void addTemplate(String keyword, int maxSpeed, int mass, int maxBrake, int power) {
         templates.add(new DataTemplate(keyword, maxSpeed, mass, maxBrake, power));
     }
     
+    /**
+     * Adds a data template and returns the instance of it.
+     * @param keyword
+     * @param maxSpeed
+     * @param mass
+     * @param maxBrake
+     * @param power
+     * @return The instance of the data template.
+     */
     private DataTemplate addTemplateR(String keyword, int maxSpeed, int mass, int maxBrake, int power) {
         DataTemplate dt = new DataTemplate(keyword, maxSpeed, mass, maxBrake, power);
         templates.add(dt);
         return dt;
     }
     
+    /**
+     * Adds a data template from a data template instance.
+     * @param dt 
+     */
     public void addTemplate(DataTemplate dt) {
         templates.add(dt);
     }
     
+    /**
+     * Returns the data template given a specific keyword.
+     * @param keyword The keyword to be searched for.
+     * @return DataTemplate
+     */
     public DataTemplate getTemplate(String keyword) {
         for(int i = 0; i < templates.size(); i++) {
             if(templates.get(i).keywordEqual(keyword)) {
