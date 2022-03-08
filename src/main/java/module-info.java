d94d0413f4a5495c99ba9b61e6bacdcaa90fe1ae
Daniel-Gill <d.gill@se20.qmul.ac.uk>
2022-03-08 20:49:03 +0000

    Added module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module net.danielgill.ros.timetable {
+    exports net.danielgill.ros.timetable;
+    exports net.danielgill.ros.timetable.data;
+    exports net.danielgill.ros.timetable.event;
+    exports net.danielgill.ros.timetable.location;
+    exports net.danielgill.ros.timetable.parse;
+    exports net.danielgill.ros.timetable.reference;
+    exports net.danielgill.ros.timetable.service;
+    exports net.danielgill.ros.timetable.template;
+    exports net.danielgill.ros.timetable.time;
+}
